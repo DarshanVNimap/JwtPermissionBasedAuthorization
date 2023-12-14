d3e93e3506f1e058bd25da8b1f81fe68175f77e1
DarshanVNimap <darshanwala@nimapinfotech.com>
2023-12-14 18:39:58 +0530

    learn actuator

--- src/main/java/com/jwtTokenPermissionAuth/JwtUtils/JwtService.java
+++ src/main/java/com/jwtTokenPermissionAuth/JwtUtils/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
 
 		return Jwts.builder().setClaims(extraClaim).setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 ))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
 				.signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
 	}
 
